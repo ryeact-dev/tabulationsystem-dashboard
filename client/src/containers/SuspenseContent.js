483d2e144a71fe40931a72e31fc0c0a65636c5b5
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-08-21 15:32:04 +0800

    Update SuspenseContent.js
    added spinner

--- client/src/containers/SuspenseContent.js
+++ client/src/containers/SuspenseContent.js
@@ -1,7 +1,9 @@
+import LoadingSpinner from "../components/LoadingSpinner";
+
 function SuspenseContent() {
   return (
-    <div className="w-full h-screen text-gray-300 night:text-gray-200 bg-base-100">
-      Loading...
+    <div className='w-full h-screen text-gray-300 night:text-gray-200 bg-base-100'>
+      <LoadingSpinner />
     </div>
   );
 }
