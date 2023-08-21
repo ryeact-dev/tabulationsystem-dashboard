a8feb40e9bdd9c860edc44631d8abc13f51a7271
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-08-21 15:27:15 +0800

    Update index.js
    forgot to import useEffect and useCallback

--- client/src/features/results/finalists/index.js
+++ client/src/features/results/finalists/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { usersStore } from "../../../app/store";
 import { getScoresheets } from "../../../api/scoresheetsApi";
