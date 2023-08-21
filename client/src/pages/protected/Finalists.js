d09d1106dfff2c7ef1790db7c63e91a51de8c2e1
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-08-21 13:31:03 +0800

    Update Finalists.js

--- client/src/pages/protected/Finalists.js
+++ client/src/pages/protected/Finalists.js
@@ -1,21 +1,15 @@
 import { useEffect } from "react";
-import { headerStore, usersStore } from "../../app/store";
+import { headerStore } from "../../app/store";
 import Finalists from "../../features/results/finalists";
-import ErrorPage from "./404";
 
 function InternalPage() {
-  const currentUser = usersStore((state) => state.currentUser);
   const setPageTitle = headerStore((state) => state.setPageTitle);
 
-  const isAllowed = currentUser.role !== "admin";
-  const headerTitle = isAllowed ? "Not Authorize" : "Results";
-  const AllowPage = isAllowed ? <ErrorPage /> : <Finalists />;
-
   useEffect(() => {
-    setPageTitle({ title: headerTitle });
-  }, [setPageTitle, headerTitle]);
+    setPageTitle({ title: "Results" });
+  }, [setPageTitle]);
 
-  return AllowPage;
+  return <Finalists />;
 }
 
 export default InternalPage;
