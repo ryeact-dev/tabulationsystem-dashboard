84ec75eaf5175b79058d9b32add51f49a189d683
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-08-21 15:40:19 +0800

    Update index.js

--- client/src/components/LoadingSpinner/index.js
+++ client/src/components/LoadingSpinner/index.js
@@ -1,6 +1,6 @@
 export default function LoadingSpinner() {
   return (
-    <div className='flex items-center justify-center'>
+   <div className='flex my-auto items-center justify-center w-full h-full rounded-xl'>
       <svg
         className='animate-spin my-5 h-8 w-8 text-white'
         xmlns='http://www.w3.org/2000/svg'
