e0c84184d1a48470be6e6494d726977a9ea7a823
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-08-21 13:31:44 +0800

    Update ScoresByCompetition.js

--- client/src/pages/protected/ScoresByCompetition.js
+++ client/src/pages/protected/ScoresByCompetition.js
@@ -1,21 +1,15 @@
 import { useEffect } from "react";
-import { headerStore, usersStore } from "../../app/store";
+import { headerStore } from "../../app/store";
 import ScoresByCompetition from "../../features/results/scoresByCompetition";
-import ErrorPage from "./404";
 
 function InternalPage() {
-  const currentUser = usersStore((state) => state.currentUser);
   const setPageTitle = headerStore((state) => state.setPageTitle);
 
-  const isAllowed = currentUser.role !== "admin";
-  const headerTitle = isAllowed ? "Not Authorize" : "Results";
-  const AllowPage = isAllowed ? <ErrorPage /> : <ScoresByCompetition />;
-
   useEffect(() => {
-    setPageTitle({ title: headerTitle });
-  }, [setPageTitle, headerTitle]);
+    setPageTitle({ title: "Results" });
+  }, [setPageTitle]);
 
-  return AllowPage;
+  return <ScoresByCompetition />;
 }
 
 export default InternalPage;
