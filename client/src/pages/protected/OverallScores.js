cbee678b20eb5ad49b42725d02fae94de0b32fa7
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-08-21 13:31:21 +0800

    Update OverallScores.js

--- client/src/pages/protected/OverallScores.js
+++ client/src/pages/protected/OverallScores.js
@@ -1,21 +1,15 @@
 import { useEffect } from "react";
-import { headerStore, usersStore } from "../../app/store";
+import { headerStore } from "../../app/store";
 import Overall from "../../features/results/overall";
-import ErrorPage from "./404";
 
 function InternalPage() {
-  const currentUser = usersStore((state) => state.currentUser);
   const setPageTitle = headerStore((state) => state.setPageTitle);
 
-  const isAllowed = currentUser.role !== "admin";
-  const headerTitle = isAllowed ? "Not Authorize" : "Results";
-  const AllowPage = isAllowed ? <ErrorPage /> : <Overall />;
-
   useEffect(() => {
-    setPageTitle({ title: headerTitle });
-  }, [setPageTitle, headerTitle]);
+    setPageTitle({ title: "Results" });
+  }, [setPageTitle]);
 
-  return AllowPage;
+  return <Overall />;
 }
 
 export default InternalPage;
