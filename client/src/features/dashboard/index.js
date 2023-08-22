0b8435abd245d23f33ea138e6028adf8d25f7f22
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-08-22 11:22:49 +0800

    Update index.js
    update the dashboard code

--- client/src/features/dashboard/index.js
+++ client/src/features/dashboard/index.js
@@ -1,5 +1,15 @@
-function Dashboard() {
-  return <h1>Tarugo Team Dashboard</h1>;
-}
+import TitleCard from "../../components/Cards/TitleCard";
 
-export default Dashboard;
+export default function Dashboard() {
+  return (
+    <TitleCard>
+      <section className="flex flex-col justify-center items-center gap-2">
+        <h1 className="text-primary text-4xl font-bold">Tabulation System</h1>
+        <p className="text-xl">
+          Powered by <span className="font-semibold">React JS</span> and
+          <span className="font-bold"> Daisy Ui</span>
+        </p>
+      </section>
+    </TitleCard>
+  );
+}
