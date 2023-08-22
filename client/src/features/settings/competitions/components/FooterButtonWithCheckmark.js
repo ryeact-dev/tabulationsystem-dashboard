8adcef09c7b52ff953397434a682e3cdda1c372a
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-08-22 19:01:39 +0800

    Update FooterButtonWithCheckmark.js

--- client/src/features/settings/competitions/components/FooterButtonWithCheckmark.js
+++ client/src/features/settings/competitions/components/FooterButtonWithCheckmark.js
@@ -1,40 +1,40 @@
 export default function FooterButtonWithCheckmark({
-  extraObject,
   competitionObj,
   updateFormValue,
   closeModal,
   saveCompetition,
   isLoading,
+  competitionId,
 }) {
   const btnNameUpdate = isLoading ? "Updating" : "Update";
   const btnNameNew = isLoading ? "Saving" : "Save";
 
   const content = (
-    <footer className='flex justify-between items-center'>
-      <label className='label cursor-pointer'>
-       <input
+    <footer className="flex justify-between items-center">
+      <label className="label cursor-pointer">
+        <input
           type="checkbox"
           className="checkbox checkbox-primary"
           defaultChecked={competitionObj.is_finalist}
           onChange={() =>
             updateFormValue({
-              updateType: "isFinalist",
+              updateType: "is_finalist",
               value: !competitionObj.is_finalist,
             })
           }
         />
-        <span className='label-text ml-2'>Finalist's Competition</span>
+        <span className="label-text ml-2">Finalist's Competition</span>
       </label>
-      <div className='modal-action mt-0'>
-        <button className='btn-ghost btn' onClick={() => closeModal()}>
+      <div className="modal-action mt-0">
+        <button className="btn-ghost btn" onClick={() => closeModal()}>
           Cancel
         </button>
         <button
-          className='btn-primary btn px-6 text-white'
+          className="btn-primary btn px-6 text-white"
           onClick={() => saveCompetition()}
           disabled={isLoading}
         >
-          {extraObject ? btnNameUpdate : btnNameNew}
+          {competitionId ? btnNameUpdate : btnNameNew}
         </button>
       </div>
     </footer>
