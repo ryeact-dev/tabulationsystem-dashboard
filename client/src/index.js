5ae5e7244e524e6f03e0a36272029e45524be833
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-08-22 11:13:37 +0800

    Update index.js
    update the suspense to add skeleton

--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,17 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import SkeletonLandingPage from "./components/Skeletons/SkeletonLandingPage";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Suspense>
+    <Suspense fallback={<SkeletonLandingPage />}>
       <QueryClientProvider client={queryClient}>
         <App />
-        {/* <ReactQueryDevtools initialIsOpen /> */}
       </QueryClientProvider>
     </Suspense>
   </React.StrictMode>
